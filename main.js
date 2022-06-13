65ae3bd905263a059b269bb3097c90a6db138664
jeongunho <unhojeong@naver.com>
2022-06-13 18:03:43 +0900

    Edit

--- main.js
+++ main.js
@@ -1,57 +1,47 @@
 'use strict';
 
-// fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
-
-// axios.get('https://jsonplaceholder.typicode.com/users?id=1')
+// axios.get('https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=f62b1e4c6bc321e334b1eb243e7fded5')
 //   .then(function (response) {
-//     // handle success
-//     console.log(response, 'success');
+//     console.log(response);
 //   })
 //   .catch(function (error) {
-//     // handle error
-//     console.log(error, 'error');
+//     console.log(error);
+//   })
+//   .then(function () {
 //   });
 
-// axios.get('https://jsonplaceholder.typicode.com/users', {params: {id : 1}})
-// .then(function (response) {
-//   // handle success
-//   console.log(response, 'success');
-// })
-// .catch(function (error) {
-//   // handle error
-//   console.log(error, 'error');
-// });
-
 const API_KEY = 'f62b1e4c6bc321e334b1eb243e7fded5';
-const feelLikeDisplay = document.querySelector('.feel-like > span');
-const windDisplay = document.querySelector('.wind > span');
+const selectBox = document.querySelector('#select-box');
+const locatioDisplay = document.querySelector('.location');
+const tempratureDisplay = document.querySelector('.temperature > span');
 const weatherDisplay = document.querySelector('.weather > img');
-const locationDisplay = document.querySelector('.location');
-const temperatureDisplay = document.querySelector('.temperature > span');
-const weatherSelect = document.querySelector('#weather-select');
-const info = document.querySelector('.info');
+const feelsLikeDisplay = document.querySelector('.feelsLike > span');
+const windDisplay = document.querySelector('.wind > span');
+const forecastDisplay = document.querySelector('.forecast');
 
-weatherSelect.addEventListener('change', (e) => {
-  getWeather(e.currentTarget.value);
-})
+document.addEventListener('DOMContentLoaded', () => {
+  transformInfo('seoul');
+});
+selectBox.addEventListener('change', (e) => {
+  transformInfo(e.currentTarget.value);
+});
 
-const getWeather = async (city='seoul') => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+const transformInfo = async (value) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${API_KEY}`;
   const response = await axios.get(url);
-  const { name, main, weather, wind } = response.data;
+  const {main, name, weather, wind} = response.data;
 
-  locationDisplay.textContent = name;
-  temperatureDisplay.textContent = transferTemp(main.temp);
-  weatherDisplay.setAttribute('src', transferIcon(weather[0].icon));
-  feelLikeDisplay.textContent = transferTemp(main.feels_like);
+  locatioDisplay.textContent = name;
+  tempratureDisplay.textContent = transformTemp(main.temp);
+  feelsLikeDisplay.textContent = transformTemp(main.feels_like);
   windDisplay.textContent = wind.speed;
-};
+  weatherDisplay.setAttribute('src', transformIcon(weather[0].icon));
+  console.log(response);
+}
 
-const transferTemp = (temp) => {
+const transformTemp = (temp) => {
   return (temp - 273.15).toFixed(1);
 }
-const transferIcon = (icon) => {
+const transformIcon = (icon) => {
   return `http://openweathermap.org/img/wn/${icon}@2x.png`
 }
